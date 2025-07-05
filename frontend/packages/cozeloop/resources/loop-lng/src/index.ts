202fc7a1a3458bd48648ae1ad8c9da864eb5d1e6
qihai <qihai@bytedance.com>
2025-07-06 00:55:09 +0800

    feat(loop-lng): license header

--- frontend/packages/cozeloop/resources/loop-lng/src/index.ts
+++ frontend/packages/cozeloop/resources/loop-lng/src/index.ts
@@ -1,3 +1,5 @@
+// Copyright (c) 2025 Bytedance Ltd. and/or its affiliates
+// SPDX-License-Identifier: Apache-2.0
 import localeZhCN from './locales/zh-CN.json';
 import localeEn from './locales/en-US.json';
 
