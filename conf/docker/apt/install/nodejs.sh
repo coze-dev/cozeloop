27a21485c6f076c555231c637ba9280beac5a174
JellyTony <bytedev@qq.com>
2025-07-26 20:05:37 +0800

    refactor(docker): 移除平台限制并添加多架构nodejs支持
    移除docker-compose.yml中app服务的平台限制
    修改nodejs安装脚本以支持x86和arm架构

--- conf/docker/apt/install/nodejs.sh
+++ conf/docker/apt/install/nodejs.sh
@@ -2,11 +2,26 @@
 
 set -ex
 
+# 检测系统架构
+ARCH=$(uname -m)
+case $ARCH in
+    x86_64)
+        NODE_ARCH="x64"
+        ;;
+    aarch64|arm64)
+        NODE_ARCH="arm64"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+
 cd /tmp
-curl -LO https://nodejs.org/dist/v20.13.1/node-v20.13.1-linux-x64.tar.xz
-tar -xJf node-v20.13.1-linux-x64.tar.xz
-mv node-v20.13.1-linux-x64 /usr/local/nodejs
-rm node-v20.13.1-linux-x64.tar.xz
+curl -LO https://nodejs.org/dist/v20.13.1/node-v20.13.1-linux-${NODE_ARCH}.tar.xz
+tar -xJf node-v20.13.1-linux-${NODE_ARCH}.tar.xz
+mv node-v20.13.1-linux-${NODE_ARCH} /usr/local/nodejs
+rm node-v20.13.1-linux-${NODE_ARCH}.tar.xz
 
 ln -s /usr/local/nodejs/bin/node /usr/bin/node
 ln -s /usr/local/nodejs/bin/npm /usr/bin/npm
